8fbc9136ea78686aa9420f748d65b9101557543c
Doomaykaka <73484636+Doomaykaka@users.noreply.github.com>
2022-07-07 11:10:34 +0300

    First task

--- FirstTask/src/Source/Main.java
+++ FirstTask/src/Source/Main.java
@@ -1,6 +1,6 @@
 package Source;
 
-public class Main  extends Authentication {
+public class Main extends Authentication {
 	//точка входа в приложение
 	public static void main(String[] args) {	
 		Authentication.athentificateReadAccDat(); //Вызываем метод athentificate для получения списка администраторов
