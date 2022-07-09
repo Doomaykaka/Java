48f6644770f8d91b8bfb5d217ea5d40c323e4735
Doomaykaka <73484636+Doomaykaka@users.noreply.github.com>
2022-07-09 22:34:34 +0300

    Second lesson

--- Second/src/Source/Main.java
+++ Second/src/Source/Main.java
@@ -4,7 +4,7 @@ public class Main extends Authentication {
 
 	public static void main(String[] args) {
 		Authentication aut = new Authentication(); //Создаём объект аутентификации (экземпляр класса Authentication)
-		aut.athentificate(); //Вызываем метод athentificate
+		aut.athentificate(); //Вызываем метод athentificate 
 	}
 
 }
